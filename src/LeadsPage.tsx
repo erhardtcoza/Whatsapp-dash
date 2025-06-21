a247a5848c9477325f52da283f485242760473f4
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:47:44 +0200

    Create LeadsPage.tsx

---
+++ src/LeadsPage.tsx
@@ -0,0 +1,9 @@
+// src/LeadsPage.tsx
+export default function LeadsPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Leads</h2>
+      <div>Leads content goes here...</div>
+    </div>
+  );
+}
