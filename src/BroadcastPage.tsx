2d6f71f6718caeef435d04ccd7b2591a9eda0330
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:48:33 +0200

    Create BroadcastPage.tsx

---
+++ src/BroadcastPage.tsx
@@ -0,0 +1,9 @@
+// src/BroadcastPage.tsx
+export default function BroadcastPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Broadcast</h2>
+      <div>Broadcast content goes here...</div>
+    </div>
+  );
+}
