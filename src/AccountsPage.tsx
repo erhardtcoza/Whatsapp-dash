8202fa4517f30cf662fe245fa84212f8f888241a
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 09:08:01 +0200

    Update AccountsPage.tsx

--- src/AccountsPage.tsx
+++ src/AccountsPage.tsx
@@ -1,5 +1,5 @@
 type Props = { colors: any; darkMode: boolean };
-export default function AccountsPage({ colors, darkMode }: Props) {
+export default function AccountsPage({ colors }: Props) {
   return (
     <div style={{
       color: colors.text,
