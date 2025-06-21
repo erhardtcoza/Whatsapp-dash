b9e3c458baa26aa9e01a44502457c05bdd22fc84
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:51:39 +0200

    Create AddUserPage.tsx

---
+++ src/AddUserPage.tsx
@@ -0,0 +1,9 @@
+// src/AddUserPage.tsx
+export default function AddUserPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>AddUser</h2>
+      <div>AddUser content goes here...</div>
+    </div>
+  );
+}
