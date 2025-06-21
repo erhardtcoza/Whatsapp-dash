bacbe92e3a6e5014612439a95dd13df12cb2be30
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:45:20 +0200

    Create SupportPage.tsx

---
+++ src/SupportPage.tsx
@@ -0,0 +1,9 @@
+// src/SupportPage.tsx
+export default function SupportPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Support</h2>
+      <div>Support content goes here...</div>
+    </div>
+  );
+}
