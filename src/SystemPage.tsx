4200e237f81c22106e1b6c24b0518f95832219c3
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:51:00 +0200

    Create SystemPage.tsx

---
+++ src/SystemPage.tsx
@@ -0,0 +1,9 @@
+// src/SystemPage.tsx
+export default function SystemPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>System</h2>
+      <div>System content goes here...</div>
+    </div>
+  );
+}
