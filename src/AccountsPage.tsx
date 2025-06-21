5e9de7df9c8325adfa1f1511587e0e483fab8d61
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:46:18 +0200

    Create AccountsPage.tsx

---
+++ src/AccountsPage.tsx
@@ -0,0 +1,9 @@
+// src/AccountsPage.tsx
+export default function AccountsPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Accouns</h2>
+      <div>Accounts content goes here...</div>
+    </div>
+  );
+}
