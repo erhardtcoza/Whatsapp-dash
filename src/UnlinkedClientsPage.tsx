65b9669776dcd682f3fe26cbeb0361de9cc7a17e
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:52:22 +0200

    Create UnlinkedClientsPage.tsx

---
+++ src/UnlinkedClientsPage.tsx
@@ -0,0 +1,9 @@
+// src/UnlinkedClientsPage.tsx
+export default function UnlinkedClientsPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>UnlinkedClients</h2>
+      <div>UnlinkedClients content goes here...</div>
+    </div>
+  );
+}
