aa7a5712e46d8dc46b045b84a91c156d40b19bbf
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:50:21 +0200

    Create OfficeHoursPage.tsx

---
+++ src/OfficeHoursPage.tsx
@@ -0,0 +1,9 @@
+// src/OfficeHoursPage.tsx
+export default function OfficeHoursPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>OfficeHours</h2>
+      <div>OfficeHours content goes here...</div>
+    </div>
+  );
+}
