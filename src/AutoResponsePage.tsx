59b7228d3bd65214d85ced4975f652ec97f981d9
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-21 08:49:41 +0200

    Create AutoResponsePage.tsx

---
+++ src/AutoResponsePage.tsx
@@ -0,0 +1,9 @@
+// src/AutoResponsePage.tsx
+export default function AutoResponsePage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>AutoResponse</h2>
+      <div>AutoResponse content goes here...</div>
+    </div>
+  );
+}
