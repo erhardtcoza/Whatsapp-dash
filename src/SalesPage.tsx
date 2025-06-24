cedee54947a7ad258cbcddd197a1a8089c863c6c
Erhardt <84268153+erhardtcoza@users.noreply.github.com>
2025-06-24 22:28:06 +0200

    Update SalesPage.tsx

--- src/SalesPage.tsx
+++ src/SalesPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { API_BASE }           from './config';
 
 export default function SupportPage({ colors }: any) {
   const [chats, setChats] = useState<any[]>([]);
